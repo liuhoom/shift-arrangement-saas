d9616e0b9c2c18a2972607b77d2053017c7d935d
pb.liu <liuhoom@gmail.com>
2025-01-21 10:56:51 +0800

    init repo

---
+++ config/index.ts
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+
+export const siteConfig: Metadata = {
+  title: '排班系统-SAAS',
+  description: '你的专属排班神器',
+  keywords: [
+    'shift arrangement',
+    'work schedule',
+    'saas',
+    'nextjs',
+    'tailwind',
+    'shadcn',
+    'typescript',
+  ] as Array<string>,
+  authors: {
+    name: 'liuhoom',
+    url: 'https://github.com/liuhoom',
+  },
+} as const
+
+export const links = {
+  sourceCode: 'https://github.com/liuhoom/shift-arrangement-saas',
+} as const
